4ba8a673dc02c6d45e27d8b3484bfd24b2b450f3
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2026-03-13 23:44:09 +0100

    feat: add version command to display version information

---
+++ cmd/version.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display detailed version information.",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("Version:   ", rootCmd.Version)
+		fmt.Println("Go version:", runtime.Version())
+		fmt.Println("OS/Arch:   ", runtime.GOOS+"/"+runtime.GOARCH)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+	_ = versionCmd.InheritedFlags().MarkHidden("kubeconfig")
+	_ = versionCmd.InheritedFlags().MarkHidden("request-timeout")
+}
